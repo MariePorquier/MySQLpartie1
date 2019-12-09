de347507129a49144871902de6c961e3ebb32a9a
MariePorquier <marieporquier@free.fr>
2019-12-09 09:34:36 +0100

    sqlpartie1

--- script.sql
+++ script.sql
@@ -2,12 +2,13 @@
 --Exo1
 --Création d'une base de donnée languages
 CREATE DATABASE `languages`;
--- Création de la BD languages si elle n'esxiste pas  (bonne pratique!)
+-- Création de la BD languages si elle n'existe pas  (bonne pratique!)
 CREATE DATABASE IF NOT EXISTS `languages`;
 --Partie1
 --Exo2
 -- Créer une base de données webDevelopment avec l’encodage UTF-8.
-CREATE DATABASE `webDevelopment` CHARACTER SET `utf8`;
+CREATE DATABASE `webDevelopment` CHARACTER SET 'utf8';
+CREATE DATABASE `webDevelopment` CHARSET='utf8';
 --Partie1
 --Exo3
 --Créer une base de données frameworks avec l’encodage UTF-8 si elle n’existe pas.
